237cb4aefdc408abb5da58b4b92cc05ecb241c56
ayushijain-iuc <ayushijain.jain12345@gmail.com>
2024-09-02 13:14:49 +0530

    fourth commit

--- ocr_converter/urls.py
+++ ocr_converter/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/',include("ocr.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
